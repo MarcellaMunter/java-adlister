d22a6cc00bc89a50cc7f88e9314f216924ee8507
mdm911 <mdm911@yahoo.com>
2018-04-05 17:03:08 -0500

    In progress passwords exercise

--- src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/CreateAdServlet.java
@@ -15,6 +15,12 @@ public class CreateAdServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/ads/create.jsp")
             .forward(request, response);
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/ads/create.jsp").forward(request, response);
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -25,5 +31,6 @@ public class CreateAdServlet extends HttpServlet {
         );
         DaoFactory.getAdsDao().insert(ad);
         response.sendRedirect("/ads");
+
     }
 }
